7d214834f8e85d230d3a343ec27146a7dabbe701
sergpetrov <s.a.petrov.spb@gmail.com>
2025-07-21 14:20:53 +0100

    Test

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,12 @@
 import { Hono } from "hono";
+import terms from "../react-app/terms/index.html?raw";
+
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+
+app.get("/terms", (c) => {
+  return c.html(terms);
+});
 
 export default app;
